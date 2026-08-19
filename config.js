201c6b12309d0ef3908c7514ac625f433af3ef2b
J_attaphon <52853486+J-attapon@users.noreply.github.com>
2026-08-19 10:12:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@
 window.RUBBER_CONFIG = {
   SUPABASE_URL: 'https://jonrcmwxruyaulhyeyap.supabase.co',
   SUPABASE_PUBLISHABLE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd0a2tjYWFxemVuc2d3ZmRnbHJ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIwNTg4NzMsImV4cCI6MjA2NzYzNDg3M30.1D1pWd45-n8m-jHn9j79Ym0XGzJ_nF7qg4DkJ1X317M',
-  AUTH_DOMAIN: '',
+  AUTH_DOMAIN: 'gmail',
   GRADES: ['3', '4', '5', 'ฟอง', 'คัดทิ้ง'],
   VERSION: '7.0.0'
 };
